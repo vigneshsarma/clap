3eb1c8969c5e23e2e7f0a8b48c0cf94792cded16
Vignesh Sarma K <vignesh@recruiterbox.com>
2015-01-03 22:53:25 +0530

    updated README and example

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # Clap.Cl
+CLAP is a port cum re-imagining of Clojure library [tools.cli](https://github.com/clojure/tools.cli/). I found most of the existing tools for the purpose to be old and unmaintained.
 
 ## Usage
 
+You can set up your script like so.
+```lisp
+(in-package :cl-user)
+
+(defpackage my-program
+  (:use :cl :clap ))
+
+(in-package :my-program)
+
+(defparameter cli-options2
+  '(("-p" "--port" "Port number for the server."
+          :required "PORT"
+          :default 80)
+    ("-v" nil "Verbosity"
+          :id :verbosity
+          :default 0)
+    ("-h" "--help" "Print help message and exit")))
+
+(clap:parse-opts (clap:get-argv) cli-options)
+```
+
+Execute the command line:
+
+    my-program -vvvp8080 foo --help --invalid-opt
+
+to produce the map:
+```lisp
+((:options    ((:port 8080)
+               (:verbosity 3)
+               (:help true)))
+  (:arguments ("foo"))
+  (:summary   "  -p, --port PORT  80  Port number
+                 -v                   Verbosity level
+                 -h, --help")
+  (:errors    ("Unknown option: \"--invalid-opt\"")))
+```
+You can optionaly give a keyword argument `:handler-fn` to parse-opts which takes as arguemnts `options, arguments, summary, errors`. Check out `example.lisp` to for a working example.
+
+
 ## Installation
+Right now this is not part of quicklisp so the easiest way to install it is in `shell`:
+```bash
+$ cd QUICKLISP_FOLDER/local-projects
+$ git clone git@github.com:vigneshsarma/clap.git
+```
+go to quicklisp loaded lisp `repl`:
+
+    (ql:quickload :clap)
+
+should now work
--- example.lisp
+++ example.lisp
@@ -1,20 +1,44 @@
+#!/usr/bin/env sbcl --script
+
+(load "~/.sbclrc")
+(ql:quickload "clap" :verbose nil)
+
 (in-package :cl-user)
 (defpackage clap-ql
-  (:use :cl :clap)
-  ;; (:export :tokenize-args)
-  )
+  (:use :cl :clap :ql))
 (in-package :clap-ql)
 
 (defparameter cli-options
-  `(((:short-opt "-p")
-     (:long-opt "--port")
-     (:required t)
-     (:default 80))
-    ((:short-opt "-v")
-     (:required t)
-     (:id :verbosity)
-     (:default 0))
-    ((:short-opt "-h")
-     (:long-opt "--help"))))
+  '(("-s" "--search" "Search for a given system."
+     :required "SYSTEM")
+    ("-i" "--install" "Install the given system."
+     :required "SYSTEM")
+    ("-u" "--update-all-dists" "Update the default dist.")
+    ("-U" "--update-client" "Update the client.")
+    ("-v" nil "Verbosity"
+     :required t
+     :id :verbosity
+     :default 0)
+    ("-h" "--help" "Print help message and exit")))
 
-(parse-opts cli-options)
+(defun main (options arguments summary errors)
+  (labels ((get-value (key)
+             (cdr (assoc key options))))
+   (cond
+    (errors
+     (format t "~{~a~%~}" errors))
+    ((get-value :install)
+     (ql:quickload (get-value :install)))
+    ((get-value :search)
+     (ql:system-apropos (get-value :search)))
+    ((get-value :update-all-dists)
+     (ql:update-all-dists))
+    ((get-value :update-client)
+     (ql:update-client))
+    ((get-value :help)
+     (progn
+       (format t "Usage: ./example.lisp [options] arguments~%~%")
+       (format t "This is a command line interface to quicklisp.~%~%")
+       (princ summary))))))
+
+(clap:parse-opts (clap:get-argv) cli-options :handler-fn #'main)
